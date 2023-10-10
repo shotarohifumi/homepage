2f125d6b3c93aef2404750c5f247a92bea9c835d
DESKTOP-4G3E252\社員１ <lotus.life13@gmail.com>
2023-10-10 16:10:49 +0900

    ボタン

--- script.js
+++ script.js
@@ -1,2 +1,61 @@
-// $(`#toplogo`).rotate3Di(180, 3000);
+$(function () {
+    // $(`button`).on(`mouseover`, function () {
+    //     $(this).css(`opacity`, 0.5);
+    // })
+    // $(`button`).on(`mouseleave`, function () {
+    //     $(this).css(`opacity`, 1);
+    // })
 
+
+
+    // 案1
+    // $('#dayoff').hover(function () {
+    //     $(this).animate({
+    //         // width: `+=200`,
+    //         height: 40,
+    //         width: 104,
+    //         opacity: 0.5,
+    //         margin: '+=10',
+    //     }, 1000)
+    //         .animate({
+    //             // width: `-=200`,
+    //             height: 48,
+    //             width: 128,
+    //             opacity: 1,
+    //             margin: '-=10'
+    //         },1000)
+    // });
+    // $('#dayoff').mouseleave(function(){
+    //     $('#dayoff').stop(false, true);
+    // });
+    // 案２
+    $('button').mouseover(function(){
+        // $(this).hide({
+        //     'display': 'none'
+        // });
+        $(this).animate({
+            'width': '104px',
+            'height': '40px',
+            'opacity': '0.5',
+            'font-size': '12px',
+            'repeat': 'infinity',
+            // 'margin': '+=10'
+        }, 'fast');
+    });
+    $('button').mouseout(function(){
+        $(this).animate({
+            'height': '48px',
+            'width': '128px',
+            'opacity': '1',
+            // 'margin': '-=10'
+        }, 'fast');
+    });
+    // 案３
+    // var handleHoverIn = function() {
+    //     $('#dayoff').css('width', '104px');
+    // }
+    // var handleHoverOut = function() {
+    //     $('#dayoff').css('width', '128px');
+    // }
+    // $('#dayoff').hover(handleHoverIn, handleHoverOut);
+});
