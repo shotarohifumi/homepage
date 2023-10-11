64dfdb4e807d56a6da5567ccc76430a0fb2a3fba
DESKTOP-4G3E252\社員１ <lotus.life13@gmail.com>
2023-10-11 13:12:53 +0900

    10/11㈬前半

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ $(function () {
     //     $('#dayoff').stop(false, true);
     // });
     // 案２
-    $('button').mouseover(function(){
+    $('#dayoff').mouseover(function(){
         // $(this).hide({
         //     'display': 'none'
         // });
@@ -39,17 +39,93 @@ $(function () {
             'opacity': '0.5',
             'font-size': '12px',
             'repeat': 'infinity',
+            'overflow': 'hidden'
             // 'margin': '+=10'
         }, 'slow');
     });
-    $('button').mouseout(function(){
+    $('#dayoff').mouseout(function(){
         $(this).animate({
             'height': '48px',
             'width': '128px',
             'opacity': '1',
             // 'margin': '-=10'
+            'font-size': '16px',
         }, 'fast');
     });
+    $('#address').mouseover(function(){
+        // $(this).hide({
+        //     'display': 'none'
+        // });
+        $(this).animate({
+            'width': '104px',
+            'height': '40px',
+            'opacity': '0.5',
+            'font-size': '12px',
+            'repeat': 'infinity',
+            'overflow': 'hidden'
+            // 'margin': '+=10'
+        }, 'slow');
+    });
+    $('#address').mouseout(function(){
+        $(this).animate({
+            'height': '48px',
+            'width': '128px',
+            'opacity': '1',
+            'font-size': '16px'
+            // 'margin': '-=10'
+        }, 'fast');
+    });
+    $('#bankaccount').mouseover(function(){
+        // $(this).hide({
+        //     'display': 'none'
+        // });
+        $(this).animate({
+            'width': '104px',
+            'height': '40px',
+            'opacity': '0.5',
+            'font-size': '12px',
+            'repeat': 'infinity',
+            'overflow': 'hidden'
+            // 'margin': '+=10'
+        }, 'slow');
+    });
+    $('#bankaccount').mouseout(function(){
+        $(this).animate({
+            'height': '48px',
+            'width': '128px',
+            'opacity': '1',
+            'font-size': '16px',
+            // 'margin': '-=10'
+        }, 'fast');
+    });
+    
+    // $('button').mouseover(function(){
+    //     // $(this).hide({
+    //     //     'display': 'block'
+    //     // });
+    //     $(this).animate({
+    //         'width': '104px',
+    //         'height': '40px',
+    //         'opacity': '0.5',
+    //         'font-size': '12px',
+    //         'repeat': 'infinity'
+    //         // 'overflow': 'hidden'
+    //         // 'margin': '+=10'
+    //         // 'transform': 'scale(0.8, 0.8)'
+    //     }, 'slow');
+    // });
+    // $('button').mouseout(function(){
+    //     $(this).animate({
+    //         'height': '48px',
+    //         'width': '128px',
+    //         'opacity': '1',
+    //         'font-size': '16px',
+    //         // 'display': 'block'
+    //         // 'overflow': 'hidden'
+    //         // 'margin': '-=10'
+    //         // 'transform': 'scale(1.0, 1.0)'
+    //     }, 'fast');
+    // });
     // 案３
     // var handleHoverIn = function() {
     //     $('#dayoff').css('width', '104px');
@@ -58,4 +134,22 @@ $(function () {
     //     $('#dayoff').css('width', '128px');
     // }
     // $('#dayoff').hover(handleHoverIn, handleHoverOut);
+    $('.test').draggable();
 });
+// SheetJSライブラリをインポート
+const XLSX = require('xlsx');
+
+// Excelファイルを読み込む
+const workbook = XLSX.readFile('ファイル名.xlsx');
+
+// シートを選択
+const sheetName = workbook.SheetNames[0]; // シート名を指定
+const sheet = workbook.Sheets[sheetName];
+
+// セルにアクセス
+const cellValue = sheet['A1'].v; // セルA1の値を取得
+
+console.log(cellValue);
+
+// 任意の後処理
+
