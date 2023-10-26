512d7e77023fb8c6dc595e78422a94da1bfe8360
PUV00014\owner <lotus.life13@gmail.com>
2023-10-26 10:29:05 +0900

    to-the-topボタン完成

--- script.js
+++ script.js
@@ -8,4 +8,23 @@ $(function() {
             $(this).removeClass('hovered');
         });
     });
+		// スクロール
+		$(window).on("scroll", function () {
+			let btn = $(".figtop").offset().top;
+
+			let position = $(window).scrollTop();
+
+			if (position >= btn) {
+					$(".figtop").removeClass("topbtn");
+			}
+			//console.log(position);
+			if (position == 0) {
+					$(".figtop").addClass("topbtn");
+			}
+    });
+
+    $(".figtop").on("click", function () {
+        $("body,html").animate({ scrollTop: 0 }, 500);
+    });
+
 });
